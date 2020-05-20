f8851c337608dfcd35aff2bac8f0b51e1170d32e
Shinan Xu <shinan.xu97@gmail.com>
2020-05-20 17:32:11 +0800

    fix sphinx markdown tables

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,9 @@ release = 'latest'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark'
+extensions = [
+    'sphinx_markdown_tables',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
