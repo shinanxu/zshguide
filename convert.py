2e1b690e87216a81c9a6c873d002dedd7ffc483b
Shinan Xu <shinan.xu97@gmail.com>
2020-05-20 14:42:16 +0800

    update doc index

--- convert.py
+++ convert.py
@@ -4,24 +4,20 @@ import glob
 import shutil
 
 ARCHIVE_DIR = './legacy'
+CONTENT_DIR = './source/content'
 EXCLUDING_FILE = ['./convert.py', './make.bat', './Makefile',
-                  './build', './source', './LICENSE', ARCHIVE_DIR]
-EXCLUDING_MD_FILE = ['./README.md', './00_Zsh-开发指南（目录）.md']
+                  './build', './source', './LICENSE', './README.md', './legacy']
+EXCLUDING_MD_FILE = ['./legacy/README.md', './legacy/00_Zsh-开发指南（目录）.md']
 
-if not os.path.exists(ARCHIVE_DIR):
-    os.makedirs(ARCHIVE_DIR)
-legacy_files = list(set(glob.glob('./*')) - set(EXCLUDING_FILE))
-content_files = list(set(glob.glob('./*.md')) - set(EXCLUDING_MD_FILE))
-
-for src in legacy_files:
-    if src in content_files:
-        chapter = os.path.basename(src)
-        ch_index = chapter[0:2]
-        ch_name = re.findall(r'（(第\S*篇-\S*)）', chapter)[0]
-        with open(src, 'r') as f:
-            content = f.readlines()
-            content[0] = f'## {ch_name}'
-        with open(f'./source/ch{ch_index}.md', 'w') as f:
-            f.writelines(content)
-    dst = os.path.join(ARCHIVE_DIR, os.path.basename(src))
-    shutil.move(src, dst)
+content_files = list(set(glob.glob(f'{ARCHIVE_DIR}/*.md')) - set(EXCLUDING_MD_FILE))
+for i in content_files:
+    chapter = os.path.basename(i)
+    ch_index = chapter[0:2]
+    ch_name = ch_index + ' ' +re.findall(r'（第\S*篇-(\S*)）', chapter)[0]
+    ch_name = ch_name.replace('-', '').lower().replace('tcp', 'TCP')
+    print(ch_name)
+    with open(i, 'r') as f:
+        content = f.readlines()
+        content[0] = f'## {ch_name}'
+    with open(f'{CONTENT_DIR}/ch{ch_index}.md', 'w') as f:
+        f.writelines(content)
--- source/index.rst
+++ source/index.rst
@@ -10,7 +10,7 @@ Welcome to zshguide's documentation!
    :maxdepth: 2
    :glob:
 
-   ./*
+   ./content/*
 
 
 .. Indices and tables
